15aa11e21ca236d518cf1bd2ebe6c10030ec84cb
Ronald Tse <ronald.tse@ribose.com>
2018-02-07 02:09:05 +0800

    Update templates to suit GB standards

--- lib/asciidoctor/gb/gbconvert.rb
+++ lib/asciidoctor/gb/gbconvert.rb
@@ -29,7 +29,7 @@ module Asciidoctor
 
       def note_label(node)
         n = get_anchors()[node["id"]]
-        return "NOTE" if n.nil?
+        return "注" if n.nil?
         n[:label]
       end
 
@@ -51,7 +51,7 @@ module Asciidoctor
           dl = t.at(".//dl")
         end
         dl
-      end 
+      end
 
       def compose_title(main, intro, part, partnumber)
         c = HTMLEntities.new
@@ -59,7 +59,7 @@ module Asciidoctor
         intro &&
           main = "#{c.encode(intro.text, :hexadecimal)}&nbsp;&mdash; #{main}"
         part &&
-          main = "#{main}&nbsp;&mdash; Part&nbsp;#{partnumber}: "\
+          main = "#{main}&nbsp;&mdash; 第#{partnumber}部: "\
           "#{c.encode(part.text, :hexadecimal)}"
         main
       end
@@ -78,38 +78,36 @@ module Asciidoctor
           gsub(/[ ]?DRAFTINFO/, meta[:draftinfo]).
           gsub(/\[TERMREF\]\s*/, "[SOURCE: ").
           gsub(/\s*\[\/TERMREF\]\s*/, "]").
-          gsub(/\s*\[ISOSECTION\]/, ", ").
-          gsub(/\s*\[MODIFICATION\]/, ", modified &mdash; ").
+          gsub(/\s*\[ISOSECTION\]/, ", 定义").
+          gsub(/\s*\[MODIFICATION\]/, ", 改写 &mdash; ").
           gsub(%r{WD/CD/DIS/FDIS}, meta[:stageabbr])
       end
 
       NORM_WITH_REFS_PREF = <<~BOILERPLATE
-          The following documents are referred to in the text in such a way
-          that some or all of their content constitutes requirements of this
-          document. For dated references, only the edition cited applies.
-          For undated references, the latest edition of the referenced
-          document (including any amendments) applies.
+          下列文件对于本文件的应用是必不可少的。
+          凡是注日期的引用文件，仅注日期的版本适用于本文件。
+          凡是不注日期的引用文件，其最新版本（包括所有的修改单）适用于本文件。
       BOILERPLATE
 
       NORM_EMPTY_PREF =
-        "There are no normative references in this document."
+        "本文件并没有规范性引用文件。"
 
       def norm_ref(isoxml, out)
-        q = "//sections/references[title = 'Normative References']"
+        q = "//sections/references[title = '规范性引用文件']"
         f = isoxml.at(ns(q)) or return
         out.div do |div|
-          clause_name("2.", "Normative References", div, false)
+          clause_name("2.", "规范性引用文件", div, false)
           norm_ref_preface(f, div)
           biblio_list(f, div, false)
         end
       end
 
       def bibliography(isoxml, out)
-        q = "//sections/references[title = 'Bibliography']"
+        q = "//sections/references[title = '参考文献']"
         f = isoxml.at(ns(q)) or return
         page_break(out)
         out.div do |div|
-          div.h1 "Bibliography", **{ class: "Section3" }
+          div.h1 "参考文献", **{ class: "Section3" }
           f.elements.reject do |e|
             ["reference", "title", "bibitem"].include? e.name
           end.each { |e| parse(e, div) }
@@ -118,9 +116,9 @@ module Asciidoctor
       end
 
       def scope(isoxml, out)
-        f = isoxml.at(ns("//clause[title = 'Scope']")) || return
+        f = isoxml.at(ns("//clause[title = '范围']")) || return
         out.div do |div|
-          clause_name("1.", "Scope", div, false)
+          clause_name("1", "范围", div, false)
           f.elements.each do |e|
             parse(e, div) unless e.name == "title"
           end
@@ -130,7 +128,7 @@ module Asciidoctor
       def terms_defs(isoxml, out)
         f = isoxml.at(ns("//terms")) || return
         out.div do |div|
-          clause_name("3.", "Terms and Definitions", div, false)
+          clause_name("3", "术语和定义", div, false)
           f.elements.each do |e|
             parse(e, div) unless e.name == "title"
           end
@@ -140,7 +138,7 @@ module Asciidoctor
       def symbols_abbrevs(isoxml, out)
         f = isoxml.at(ns("//symbols-abbrevs")) || return
         out.div do |div|
-          clause_name("4.", "Symbols and Abbreviations", div, false)
+          clause_name("4", "符号、代号和缩略语", div, false)
           f.elements.each do |e|
             parse(e, div) unless e.name == "title"
           end
@@ -148,11 +146,11 @@ module Asciidoctor
       end
 
       def introduction(isoxml, out)
-        f = isoxml.at(ns("//content[title = 'Introduction']")) || return
+        f = isoxml.at(ns("//content[title = '引言']")) || return
         title_attr = { class: "IntroTitle" }
         page_break(out)
         out.div **{ class: "Section3" } do |div|
-          div.h1 "Introduction", **attr_code(title_attr)
+          div.h1 "引言", **attr_code(title_attr)
           f.elements.each do |e|
             if e.name == "patent-notice"
               e.elements.each { |e1| parse(e1, div) }
@@ -164,10 +162,10 @@ module Asciidoctor
       end
 
       def foreword(isoxml, out)
-        f = isoxml.at(ns("//content[title = 'Foreword']")) || return
+        f = isoxml.at(ns("//content[title = '前言']")) || return
         page_break(out)
         out.div do |s|
-          s.h1 **{ class: "ForewordTitle" } { |h1| h1 << "Foreword" }
+          s.h1 **{ class: "ForewordTitle" } { |h1| h1 << "前言" }
           f.elements.each { |e| parse(e, s) unless e.name == "title" }
         end
       end
@@ -179,10 +177,10 @@ module Asciidoctor
       end
 
       def termexample_parse(node, out)
-        out.div **{ class: "Note" } do |div|
+        out.div **{ class: "注" } do |div|
           first = node.first_element_child
-          div.p **{ class: "Note" } do |p|
-            p << "EXAMPLE:"
+          div.p **{ class: "注" } do |p|
+            p << "示例:"
             insert_tab(p, 1)
             para_then_remainder(first, node, p)
           end
@@ -202,23 +200,23 @@ module Asciidoctor
       }.freeze
 
       def initial_anchor_names(d)
-        introduction_names(d.at(ns("//content[title = 'Introduction']")))
-        section_names(d.at(ns("//clause[title = 'Scope']")), "1", 1)
+        introduction_names(d.at(ns("//content[title = '引言']")))
+        section_names(d.at(ns("//clause[title = '范围']")), "1", 1)
         section_names(d.at(ns(
-          "//references[title = 'Normative References']")), "2", 1)
+          "//references[title = '规范性引用文件']")), "2", 1)
         section_names(d.at(ns("//terms")), "3", 1)
         middle_section_asset_names(d)
       end
 
       def middle_section_asset_names(d)
-        middle_sections = "//clause[title = 'Scope'] | "\
-          "//references[title = 'Normative References'] | //terms | "\
+        middle_sections = "//clause[title = '范围'] | "\
+          "//references[title = '规范性引用文件'] | //terms | "\
           "//symbols-abbrevs | //clause[parent::sections]"
         sequential_asset_names(d.xpath(ns(middle_sections)))
       end
 
       def clause_names(docxml,sect_num)
-        q = "//clause[parent::sections][not(xmlns:title = 'Scope')]"
+        q = "//clause[parent::sections][not(xmlns:title = '范围')]"
         docxml.xpath(ns(q)).each_with_index do |c, i|
           section_names(c, (i + sect_num).to_s, 1)
         end
@@ -227,9 +225,9 @@ module Asciidoctor
       def termnote_anchor_names(docxml)
         docxml.xpath(ns("//term[termnote]")).each do |t|
           t.xpath(ns("./termnote")).each_with_index do |n, i|
-            @anchors[n["id"]] = { label: "Note #{i + 1} to entry",
+            @anchors[n["id"]] = { label: "注 #{i + 1}",
                                   xref: "#{@anchors[t["id"]][:xref]},"\
-                                  "Note #{i + 1}" }
+                                  "注 #{i + 1}" }
           end
         end
       end
@@ -237,9 +235,9 @@ module Asciidoctor
       def table_note_anchor_names(docxml)
         docxml.xpath(ns("//table[note]")).each do |t|
           t.xpath(ns("./note")).each_with_index do |n, i|
-            @anchors[n["id"]] = { label: "NOTE #{i + 1}",
+            @anchors[n["id"]] = { label: "注 #{i + 1}",
                                   xref: "#{@anchors[t["id"]][:xref]},"\
-                                  "Note #{i + 1}" }
+                                  "注 #{i + 1}" }
           end
         end
       end
@@ -247,27 +245,27 @@ module Asciidoctor
       def sequential_figure_names(clause)
         i = j = 0
         clause.xpath(ns(".//figure")).each do |t|
-          label = "Figure #{i}" + ( j.zero? ? "" : "-#{j}" )
+          label = "图 #{i}" + ( j.zero? ? "" : "-#{j}" )
           if t.parent.name == "figure"
             j += 1
           else
             j = 0
             i += 1
           end
-          label = "Figure #{i}" + ( j.zero? ? "" : "-#{j}" )
+          label = "图 #{i}" + ( j.zero? ? "" : "-#{j}" )
           @anchors[t["id"]] = { label: label, xref: label }
         end
       end
 
       def sequential_asset_names(clause)
         clause.xpath(ns(".//table")).each_with_index do |t, i|
-          @anchors[t["id"]] = { label: "Table #{i + 1}",
-                                xref: "Table #{i + 1}" }
+          @anchors[t["id"]] = { label: "表 #{i + 1}",
+                                xref: "表 #{i + 1}" }
         end
         sequential_figure_names(clause)
         clause.xpath(ns(".//formula")).each_with_index do |t, i|
           @anchors[t["id"]] = { label: (i + 1).to_s,
-                                xref: "Formula #{i + 1}" }
+                                xref: "公式 #{i + 1}" }
         end
       end
 
@@ -280,25 +278,25 @@ module Asciidoctor
             j = 0
             i += 1
           end
-          label = "Figure #{num}.#{i}" + ( j.zero? ? "" : "-#{j}" )
+          label = "图 #{num}.#{i}" + ( j.zero? ? "" : "-#{j}" )
           @anchors[t["id"]] = { label: label, xref: label }
         end
       end
 
       def hierarchical_asset_names(clause, num)
         clause.xpath(ns(".//table")).each_with_index do |t, i|
-          @anchors[t["id"]] = { label: "Table #{num}.#{i + 1}",
-                                xref: "Table #{num}.#{i + 1}" }
+          @anchors[t["id"]] = { label: "表 #{num}.#{i + 1}",
+                                xref: "表 #{num}.#{i + 1}" }
         end
         hierarchical_figure_names(clause, num)
         clause.xpath(ns(".//formula")).each_with_index do |t, i|
           @anchors[t["id"]] = { label: "#{num}.#{i + 1}",
-                                xref: "Formula #{num}.#{i + 1}" }
+                                xref: "公式 #{num}.#{i + 1}" }
         end
       end
 
       def section_names(clause, num, level)
-        @anchors[clause["id"]] = { label: num, xref: "Clause #{num}",
+        @anchors[clause["id"]] = { label: num, xref: "#{num}",
                                    level: level }
         clause.xpath(ns("./subsection | ./term")).each_with_index do |c, i|
           section_names1(c, "#{num}.#{i + 1}", level + 1)
@@ -308,7 +306,7 @@ module Asciidoctor
       def section_names1(clause, num, level)
         @anchors[clause["id"]] =
           { label: num, level: level,
-            xref: clause.name == "term" ? num : "Clause #{num}" }
+            xref: clause.name == "term" ? num : "#{num}" }
         clause.xpath(ns("./subsection ")).
           each_with_index do |c, i|
           section_names1(c, "#{num}.#{i + 1}", level + 1)
@@ -316,11 +314,11 @@ module Asciidoctor
       end
 
       def annex_names(clause, num)
-        obligation = "(Informative)"
-        obligation = "(Normative)" if clause["subtype"] == "normative"
-        label = "<b>Annex #{num}</b><br/>#{obligation}"
+        obligation = "（非规范性）"
+        obligation = "（规范性）" if clause["subtype"] == "normative"
+        label = "<b>附录 #{num}</b><br/>#{obligation}"
         @anchors[clause["id"]] = { label: label,
-                                   xref: "Annex #{num}", level: 1 }
+                                   xref: "附录 #{num}", level: 1 }
         clause.xpath(ns("./subsection")).each_with_index do |c, i|
           annex_names1(c, "#{num}.#{i + 1}", 2)
         end
@@ -355,12 +353,6 @@ module Asciidoctor
       def string_parse(node, out)
         node.children.each { |c| parse(c, out) }
       end
-
-
-
-
-
-
 
 
     end
