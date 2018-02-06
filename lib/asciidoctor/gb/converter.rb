b4baf111ce306a09709ca7eac982619f057c4b7b
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-06 18:53:21 +1100

    initial population

--- lib/asciidoctor/gb/converter.rb
+++ lib/asciidoctor/gb/converter.rb
@@ -1,70 +1,33 @@
 require "asciidoctor"
-require "asciidoctor/csd/version"
-require "asciidoctor/csd/csdconvert"
+require "asciidoctor/gb/version"
+require "asciidoctor/gb/gbconvert"
 require "asciidoctor/iso/converter"
 
 module Asciidoctor
-  module Csd
-    CSD_NAMESPACE = "https://www.calconnect.org/standards/csd"
+  module Gb
+    GB_NAMESPACE = "https://www.calconnect.org/standards/csd"
 
     # A {Converter} implementation that generates CSD output, and a document
     # schema encapsulation of the document for validation
     class Converter < ISO::Converter
 
-      register_for "csd"
-
-      def metadata_author(node, xml)
-        xml.creator **{ role: "author" } do |a|
-          a.technical_committee node.attr("technical-committee"),
-            **attr_code(type: node.attr("technical-committee-type"))
-        end
-      end
-
-      def title(node, xml)
-        ["en"].each do |lang|
-          xml.title **{ language: lang } do |t|
-            t << node.attr("title")
-          end
-        end
-      end
-
-      def metadata_status(node, xml)
-        xml.status { |s| s << node.attr("status") }
-      end
-
-      def metadata_id(node, xml)
-        xml.id { |i| i << node.attr("docnumber") }
-      end
-
-      def metadata_copyright(node, xml)
-        from = node.attr("copyright-year") || Date.today.year
-        xml.copyright do |c|
-          c.from from
-          c.owner do |o|
-            o.affiliation "CalConnect"
-          end
-        end
-      end
-
-      def title_validate(root)
-        nil
-      end
+      register_for "gb"
 
       def makexml(node)
-        result = ["<?xml version='1.0' encoding='UTF-8'?>\n<csd-standard>"]
+        result = ["<?xml version='1.0' encoding='UTF-8'?>\n<gb-standard>"]
         @draft = node.attributes.has_key?("draft")
         result << noko { |ixml| front node, ixml }
         result << noko { |ixml| middle node, ixml }
-        result << "</csd-standard>"
+        result << "</gb-standard>"
         result = textcleanup(result.flatten * "\n")
         ret1 = cleanup(Nokogiri::XML(result))
-        ret1.root.add_namespace(nil, CSD_NAMESPACE)
+        ret1.root.add_namespace(nil, GB_NAMESPACE)
         ret1
       end
 
       def validate(doc)
         content_validate(doc)
-        schema_validate(doc, File.join(File.dirname(__FILE__), "csd.rng"))
+        schema_validate(doc, File.join(File.dirname(__FILE__), "gbstandard.rng"))
       end
 
       def html_doc_path(file)
@@ -72,17 +35,88 @@ module Asciidoctor
       end
 
       def doc_converter
-        CsdConvert.new(
+        GbConvert.new(
           htmlstylesheet: html_doc_path("htmlstyle.css"),
           wordstylesheet: html_doc_path("wordstyle.css"),
-          standardstylesheet: html_doc_path("csd.css"),
+          standardstylesheet: html_doc_path("gb.css"),
           header: html_doc_path("header.html"),
-          htmlcoverpage: html_doc_path("html_csd_titlepage.html"),
-          wordcoverpage: html_doc_path("word_csd_titlepage.html"),
-          htmlintropage: html_doc_path("html_csd_intro.html"),
-          wordintropage: html_doc_path("word_csd_intro.html"),
+          htmlcoverpage: html_doc_path("html_gb_titlepage.html"),
+          wordcoverpage: html_doc_path("word_gb_titlepage.html"),
+          htmlintropage: html_doc_path("html_gb_intro.html"),
+          wordintropage: html_doc_path("word_gb_intro.html"),
         )
       end
+
+      def title(node, xml)
+        ["en", "fr"].each do |lang|
+          xml.title **{ language: lang } do |t|
+            if node.attr("title-intro-#{lang}")
+              t.title_intro { |t1| t1 << node.attr("title-intro-#{lang}") }
+            end
+            t.title_main { |t1| t1 << node.attr("title-main-#{lang}") }
+            if node.attr("title-part-#{lang}")
+              t.title_part node.attr("title-part-#{lang}")
+            end
+          end
+        end
+      end
+
+      def title_intro_validate(root)
+        title_intro_en = root.at("//title[@language='en']/title-intro")
+        title_intro_zh = root.at("//title[@language='zh']/title-intro")
+        if title_intro_en.nil? && !title_intro_zh.nil?
+          warn "No English Title Intro!"
+        end
+        if !title_intro_en.nil? && title_intro_zh.nil?
+          warn "No Chinese Title Intro!"
+        end
+      end
+
+      def title_part_validate(root)
+        title_part_en = root.at("//title[@language='en']/title-part")
+        title_part_fr = root.at("//title[@language='zh']/title-part")
+        if title_part_en.nil? && !title_part_fr.nil?
+          warn "No English Title Part!"
+        end
+        if !title_part_en.nil? && title_part_fr.nil?
+          warn "No Chinese Title Part!"
+        end
+      end
+
+      def inline_quoted(node)
+        noko do |xml|
+          case node.type
+          when :emphasis then xml.em node.text
+          when :strong then xml.strong node.text
+          when :monospaced then xml.tt node.text
+          when :double then xml << "\"#{node.text}\""
+          when :single then xml << "'#{node.text}'"
+          when :superscript then xml.sup node.text
+          when :subscript then xml.sub node.text
+          when :asciimath then xml.stem node.text, **{ type: "AsciiMath" }
+          else
+            case node.role
+              # TODO localisedstring encoding for alt, deprecated
+            when "alt" then xml.admitted { |a| a << node.text }
+            when "deprecated" then xml.deprecates { |a| a << node.text }
+            when "domain" then xml.domain { |a| a << node.text }
+            when "strike" then xml.strike node.text
+            when "smallcap" then xml.smallcap node.text
+            else
+              xml << node.text
+            end
+          end
+        end.join
+      end
+
+      # TODO localisedstring encoding for preferred
+      def term_def_subclause_parse(attrs, xml, node)
+        xml.term **attr_code(attrs) do |xml_section|
+          xml_section.preferred { |name| name << node.title }
+          xml_section << node.content
+        end
+      end
+
 
 
     end
