60dacbea6a48ebad5713be79d6d50e0faad1d949
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-09 20:25:32 +1100

    code refactor

---
+++ lib/asciidoctor/gb/section.rb
@@ -0,0 +1,189 @@
+module Asciidoctor
+  module Gb
+
+    # A {Converter} implementation that generates GB output, and a document
+    # schema encapsulation of the document for validation
+    class Converter < ISO::Converter
+
+      def bibliography_parse(attrs, xml, node)
+        @biblio = true
+        xml.references **attr_code(attrs) do |xml_section|
+          title = node.level == 1 ? "参考文献" : node.title
+          xml_section.title { |t| t << title }
+          xml_section << node.content
+        end
+        @biblio = true
+      end
+
+      def term_def_parse(attrs, xml, node, title)
+        @term_def = true
+        xml.terms **attr_code(attrs) do |xml_section|
+          if title == "terms, definitions, symbols and abbreviations" # TODO: Chinese
+            title = "Terms, Definitions, Symbols and Abbreviations" # TODO: Chinese
+          else
+            title = "术语和定义"
+          end
+          xml_section.title { |t| t << title }
+          xml_section << node.content
+        end
+        @term_def = false
+      end
+
+      def norm_ref_parse(attrs, xml, node)
+        @norm_ref = true
+        xml.references **attr_code(attrs) do |xml_section|
+          xml_section.title { |t| t << "规范性引用文件" }
+          xml_section << node.content
+        end
+        @norm_ref = false
+      end
+
+      def introduction_parse(attrs, xml, node)
+        xml.introduction **attr_code(attrs) do |xml_section|
+          xml_section.title = "引言"
+          content = node.content
+          xml_section << content
+          introduction_style(node,
+                             Utils::flatten_rawtext(content).
+                             join("\n"))
+        end
+      end
+
+      def scope_parse(attrs, xml, node)
+        @scope = true
+        xml.clause **attr_code(attrs) do |xml_section|
+          xml_section.title { |t| t << "范围" }
+          content = node.content
+          xml_section << content
+          c = Utils::flatten_rawtext(content).join("\n")
+          scope_style(node, c)
+        end
+        @scope = false
+      end
+
+
+      def section(node)
+        a = { id: Asciidoctor::ISO::Utils::anchor_or_uuid(node) }
+        noko do |xml|
+          case node.title
+          when "引言" then
+            if node.level == 1
+              introduction_parse(a, xml, node)
+            else
+              clause_parse(a, xml, node)
+            end
+          when "patent notice" then patent_notice_parse(xml, node)
+          when "范围" then scope_parse(a, xml, node)
+          when "规范性引用文件" then norm_ref_parse(a, xml, node)
+          when "术语和定义"
+            term_def_parse(a, xml, node, node.title.downcase)
+          when "terms, definitions, symbols and abbreviations" # TODO: Chinese
+            term_def_parse(a, xml, node, node.title.downcase)
+          when "符号、代号和缩略语"
+            symbols_parse(a, xml, node)
+          when "参考文献" then bibliography_parse(a, xml, node)
+          else
+            if @term_def
+              term_def_subclause_parse(a, xml, node)
+            elsif @biblio
+              bibliography_parse(a, xml, node)
+            elsif node.attr("style") == "appendix" && node.level == 1
+              annex_parse(a, xml, node)
+            else
+              clause_parse(a, xml, node)
+            end
+          end
+        end.join("\n")
+      end
+
+      def preamble(node)
+        noko do |xml|
+          xml.foreword do |xml_abstract|
+            xml_abstract.title { |t| t << "前言" }
+            content = node.content
+            xml_abstract << content
+            text = Asciidoctor::ISO::Utils::flatten_rawtext(content).join("\n")
+            foreword_style(node, text)
+          end
+        end.join("\n")
+      end
+
+      # spec of permissible section sequence
+      SEQ = [
+        {
+          msg: "Initial section must be (content) Foreword",
+          val:  [{ tag: "foreword", title: "前言" }],
+        },
+        {
+          msg: "Prefatory material must be followed by (clause) Scope",
+          val:  [{ tag: "introduction", title: "引言" },
+                 { tag: "clause", title: "范围" }],
+        },
+        {
+          msg: "Prefatory material must be followed by (clause) Scope",
+          val: [{ tag: "clause", title: "范围" }],
+        },
+        # we skip normative references, it goes to end of list
+        #{
+        #msg: "Scope must be followed by Normative References",
+        #val: [{ tag: "references", title: "Normative References" }]
+        #},
+        {
+          msg: "Normative References must be followed by "\
+          "Terms and Definitions",
+          val: [
+            { tag: "terms", title: "术语和定义" },
+            { tag: "terms",
+              title: "Terms, Definitions, Symbols and Abbreviations" } # TODO: Chinese
+          ]
+        },
+      ]
+
+      def sections_sequence_validate(root)
+        puts root
+        f = root.xpath(" //foreword | //introduction | //sections/terms | "\
+                       "//sections/clause | ./references | "\
+                       "./annex")
+        names = f.map { |s| { tag: s.name, title: s.at("./title").text } }
+        names = seqcheck(names, SEQ[0][:msg], SEQ[0][:val]) or return
+        n = names[0]
+        names = seqcheck(names, SEQ[1][:msg], SEQ[1][:val]) or return
+        if n == { tag: "introduction", title: "引言" }
+          names = seqcheck(names, SEQ[2][:msg], SEQ[2][:val]) or return
+        end
+        # names = seqcheck(names, SEQ[3][:msg], SEQ[3][:val]) or return
+        names = seqcheck(names, SEQ[3][:msg], SEQ[3][:val]) or return
+        n = names.shift
+        if n == { tag: "clause", title: "符号、代号和缩略语" }
+          n = names.shift or return
+        end
+        n[:tag] == "clause" or
+          warn "ISO style: Document must contain at least one clause"
+        n == { tag: "clause", title: "Scope" } and
+          warn "ISO style: Scope must occur before Terms and Definitions"
+        n = names.shift or return
+        while n[:tag] == "clause"
+          n[:title] == "范围" and
+            warn "ISO style: Scope must occur before Terms and Definitions"
+          n[:title] == "符号、代号和缩略语" and
+            warn "ISO style: Symbols and Abbreviations must occur "\
+            "right after Terms and Definitions"
+          n = names.shift or return
+        end
+        unless n[:tag] == "annex" or n[:tag] == "references"
+          warn "ISO style: Only annexes and references can follow clauses"
+        end
+        while n[:tag] == "annex"
+          n = names.shift or return
+        end
+        n == { tag: "references", title: "规范性引用文件" } or
+          warn "ISO style: Document must include (references) Normative References"
+        n = names.shift
+        n == { tag: "references", title: "参考文献" } or
+          warn "ISO style: Final section must be (references) Bibliography"
+        names.empty? or
+          warn "ISO style: There are sections after the final Bibliography"
+      end
+    end
+  end
+end
---
+++ lib/asciidoctor/gb/xref_gen.rb
@@ -0,0 +1,153 @@
+require "isodoc"
+  
+module Asciidoctor
+  module Gb
+    # A {Converter} implementation that generates CSD output, and a document
+    # schema encapsulation of the document for validation
+    class GbConvert < IsoDoc::Convert
+          def initial_anchor_names(d)
+        introduction_names(d.at(ns("//introduction")))
+        section_names(d.at(ns("//clause[title = '范围']")), "1", 1)
+        section_names(d.at(ns(
+          "//references[title = '规范性引用文件']")), "2", 1)
+        section_names(d.at(ns("//terms")), "3", 1)
+        middle_section_asset_names(d)
+      end
+
+      def middle_section_asset_names(d)
+        middle_sections = "//clause[title = '范围'] | "\
+          "//references[title = '规范性引用文件'] | //terms | "\
+          "//symbols-abbrevs | //clause[parent::sections]"
+        sequential_asset_names(d.xpath(ns(middle_sections)))
+      end
+
+      def clause_names(docxml,sect_num)
+        q = "//clause[parent::sections][not(xmlns:title = '范围')]"
+        docxml.xpath(ns(q)).each_with_index do |c, i|
+          section_names(c, (i + sect_num).to_s, 1)
+        end
+      end
+
+      def termnote_anchor_names(docxml)
+        docxml.xpath(ns("//term[termnote]")).each do |t|
+          t.xpath(ns("./termnote")).each_with_index do |n, i|
+            @anchors[n["id"]] = { label: "注 #{i + 1}",
+                                  xref: "#{@anchors[t["id"]][:xref]},"\
+                                  "注 #{i + 1}" }
+          end
+        end
+      end
+
+      def table_note_anchor_names(docxml)
+        docxml.xpath(ns("//table[note]")).each do |t|
+          t.xpath(ns("./note")).each_with_index do |n, i|
+            @anchors[n["id"]] = { label: "注 #{i + 1}",
+                                  xref: "#{@anchors[t["id"]][:xref]},"\
+                                  "注 #{i + 1}" }
+          end
+        end
+      end
+
+      def sequential_figure_names(clause)
+        i = j = 0
+        clause.xpath(ns(".//figure")).each do |t|
+          label = "图 #{i}" + ( j.zero? ? "" : "-#{j}" )
+          if t.parent.name == "figure"
+            j += 1
+          else
+            j = 0
+            i += 1
+          end
+          label = "图 #{i}" + ( j.zero? ? "" : "-#{j}" )
+          @anchors[t["id"]] = { label: label, xref: label }
+        end
+      end
+
+      def sequential_asset_names(clause)
+        clause.xpath(ns(".//table")).each_with_index do |t, i|
+          @anchors[t["id"]] = { label: "表 #{i + 1}",
+                                xref: "表 #{i + 1}" }
+        end
+        sequential_figure_names(clause)
+        clause.xpath(ns(".//formula")).each_with_index do |t, i|
+          @anchors[t["id"]] = { label: (i + 1).to_s,
+                                xref: "公式 #{i + 1}" }
+        end
+      end
+
+      def hierarchical_figure_names(clause, num)
+        i = j = 0
+        clause.xpath(ns(".//figure")).each do |t|
+          if t.parent.name == "figure"
+            j += 1
+          else
+            j = 0
+            i += 1
+          end
+          label = "图 #{num}.#{i}" + ( j.zero? ? "" : "-#{j}" )
+          @anchors[t["id"]] = { label: label, xref: label }
+        end
+      end
+
+      def hierarchical_asset_names(clause, num)
+        clause.xpath(ns(".//table")).each_with_index do |t, i|
+          @anchors[t["id"]] = { label: "表 #{num}.#{i + 1}",
+                                xref: "表 #{num}.#{i + 1}" }
+        end
+        hierarchical_figure_names(clause, num)
+        clause.xpath(ns(".//formula")).each_with_index do |t, i|
+          @anchors[t["id"]] = { label: "#{num}.#{i + 1}",
+                                xref: "公式 #{num}.#{i + 1}" }
+        end
+      end
+
+      def section_names(clause, num, level)
+        @anchors[clause["id"]] = { label: num, xref: "#{num}",
+                                   level: level }
+        clause.xpath(ns("./subsection | ./term")).each_with_index do |c, i|
+          section_names1(c, "#{num}.#{i + 1}", level + 1)
+        end
+      end
+
+      def section_names1(clause, num, level)
+        @anchors[clause["id"]] =
+          { label: num, level: level,
+            xref: clause.name == "term" ? num : "#{num}" }
+        clause.xpath(ns("./subsection ")).
+          each_with_index do |c, i|
+          section_names1(c, "#{num}.#{i + 1}", level + 1)
+        end
+      end
+
+      def annex_names(clause, num)
+        obligation = "（非规范性）"
+        obligation = "（规范性）" if clause["subtype"] == "normative"
+        label = "<b>附录 #{num}</b><br/>#{obligation}"
+        @anchors[clause["id"]] = { label: label,
+                                   xref: "附录 #{num}", level: 1 }
+        clause.xpath(ns("./subsection")).each_with_index do |c, i|
+          annex_names1(c, "#{num}.#{i + 1}", 2)
+        end
+        hierarchical_asset_names(clause, num)
+      end
+
+      def format_ref(ref, isopub)
+        return "ISO #{ref}" if isopub
+        return "[#{ref}]" if /^\d+$/.match?(ref) && !/^\[.*\]$/.match?(ref)
+        ref
+      end
+
+      def reference_names(ref)
+        isopub = ref.at(ns(ISO_PUBLISHER_XPATH))
+        docid = ref.at(ns("./docidentifier"))
+        return ref_names(ref) unless docid
+        date = ref.at(ns("./date[@type = 'published']"))
+        reference = format_ref(docid.text, isopub)
+        reference += ": #{date.text}" if date && isopub
+        @anchors[ref["id"]] = { xref: reference }
+      end
+
+
+    end
+  end
+end
