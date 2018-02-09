968fdd9073aea7af64ae99b74f0455147e7a0af3
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-10 09:50:33 +1100

    spec script

---
+++ spec/asciidoctor/gb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Asciidoctor::Gb do
+  it "has a version number" do
+    expect(Asciidoctor::Gb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
