a318b6907cffdc92e176b836c1a8edea1a809a0f
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-08 23:22:02 +1000

    GB/T 1.1 markup; closes #57

--- lib/asciidoctor/gb/gbconvert.rb
+++ lib/asciidoctor/gb/gbconvert.rb
@@ -43,8 +43,23 @@ module Asciidoctor
 
       def cleanup(docxml)
         super
-        # intro_cleanup(docxml)
         terms_cleanup(docxml)
+        formula_cleanup(docxml)
+      end
+
+      def formula_cleanup(docxml)
+        docxml.xpath("//table[@class = 'dl']//p[not(@class)]").each do |p|
+          p["class"] = "dl"
+        end
+        docxml
+      end
+
+      def example_cleanup(docxml)
+        super
+        docxml.xpath("//table[@class = 'Note']//p[not(@class)]").each do |p|
+          p["class"] = "Note"
+        end
+        docxml
       end
 
       def term_merge(docxml, term_class)
@@ -74,7 +89,7 @@ module Asciidoctor
         term_merge(docxml, "DeprecatedTerms")
         docxml
       end
-      
+
       def intro_cleanup(docxml)
         # insert tab for purposes of ToC lining up
         docxml.xpath("//h1[@class = 'IntroTitle']").each do |h1|
@@ -132,6 +147,85 @@ module Asciidoctor
           map do |a|
           a.size < 3 ? a[0] : a[0] + termref_render(a[2])
         end.join
+      end
+
+      def formula_parse(node, out)
+        out.div **attr_code(id: node["id"], class: "formula") do |div|
+          insert_tab(div, 1)
+          parse(node.at(ns("./stem")), out)
+          insert_tab(div, 1)
+          div << "(#{get_anchors[node['id']][:label]})"
+        end
+        formula_where(node.at(ns("./dl")), out)
+      end
+
+      def formula_where(dl, out)
+        return unless dl
+        out.p { |p| p << @where_lbl }
+        formula_dl_parse(dl, out)
+      end
+
+      def formula_dl_parse(node, out)
+        out.table **{ class: "dl" } do |v|
+          node.elements.each_slice(2) do |dt, dd|
+            v.tr do |tr|
+              tr.td **{ valign: "top", align: "left" } do |term|
+                dt_parse(dt, term)
+              end
+              tr.td **{ valign: "top" } { |td| td << "&mdash;" }
+              tr.td **{ valign: "top" } do |listitem|
+                dd.children.each { |n| parse(n, listitem) }
+              end
+            end
+          end
+        end
+      end
+
+      EXAMPLE_TBL_ATTR =
+        { valign: "top", class: "example_label",
+          style: "padding:2pt 2pt 2pt 2pt" }.freeze
+
+      def example_parse(node, out)
+        out.table **attr_code(id: node["id"], class: "example") do |t|
+          t.tr do |tr|
+            tr.td **EXAMPLE_TBL_ATTR do |td|
+              td << example_label(node)
+            end
+            tr.td **{ valign: "top" } do |td|
+              node.children.each { |n| parse(n, td) }
+            end
+          end
+        end
+      end
+
+      def note_parse(node, out)
+        @note = true
+        out.table **attr_code(id: node["id"], class: "Note") do |t|
+          t.tr do |tr|
+            tr.td **EXAMPLE_TBL_ATTR do |td|
+              td << note_label(node)
+            end
+            tr.td **{ valign: "top", class: "Note" } do |td|
+              node.children.each { |n| parse(n, td) }
+            end
+          end
+        end
+        @note = false
+      end
+
+      def termnote_parse(node, out)
+        @note = true
+        out.table **attr_code(id: node["id"], class: "Note") do |t|
+          t.tr do |tr|
+            tr.td **EXAMPLE_TBL_ATTR do |td|
+              td << "#{get_anchors[node['id']][:label]}: "
+            end
+            tr.td **{ valign: "top", class: "Note" } do |td|
+              node.children.each { |n| parse(n, td) }
+            end
+          end
+        end
+        @note = false
       end
 
       def populate_template(docxml, format)
