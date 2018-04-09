03d70f19eb8adb4b9e5d94686c12bddd440f7170
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-10 00:17:52 +1000

    title spacing; code refactor

--- lib/asciidoctor/gb/gbconvert.rb
+++ lib/asciidoctor/gb/gbconvert.rb
@@ -83,7 +83,7 @@ module Asciidoctor
       def title_cleanup(docxml)
         @script == "Hans" or return
         docxml.xpath("//*[@class = 'zzContents' or @class = 'ForewordTitle' or "\
-                     "@class = 'IntroTitle] | "\
+                     "@class = 'IntroTitle'] | "\
                      "//h1[@class = 'Sections3']").each do |x|
           spaerdruck(x, false)
         end
