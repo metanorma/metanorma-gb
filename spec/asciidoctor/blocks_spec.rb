8bd0d89ff7bb312dadbbba0a0d21d4ea888720c1
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-24 22:36:38 +1000

    rspec

--- spec/asciidoctor/blocks_spec.rb
+++ spec/asciidoctor/blocks_spec.rb
@@ -22,16 +22,22 @@ RSpec.describe Asciidoctor::Gb::GbConvert do
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="" class="Note">
-                   <p class="Note"><span class="note_label">NOTE</span>&#160; These results are based on a study carried out on three different types of kernel.</p>
-                 </div>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <table class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE:</td>
+                     <td valign="top" class="Note">
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83f">These results are based on a study carried out on three different types of kernel.</p>
+       </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -60,16 +66,22 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="note1" class="Note">
-                   <p class="Note"><span class="note_label">NOTE</span>&#160; These results are based on a study carried out on three different types of kernel.</p>
-                 </div>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <table id="note1" class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE:</td>
+                     <td valign="top" class="Note">
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83f">These results are based on a study carried out on three different types of kernel.</p>
+       </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -101,19 +113,30 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="note1" class="Note">
-                   <p class="Note"><span class="note_label">NOTE  1</span>&#160; These results are based on a study carried out on three different types of kernel.</p>
-                 </div>
-                 <div id="note2" class="Note">
-                   <p class="Note"><span class="note_label">NOTE  2</span>&#160; These results are based on a study carried out on three different types of kernel.</p>
-                 </div>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <table id="note1" class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE  1:</td>
+                     <td valign="top" class="Note">
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83f">These results are based on a study carried out on three different types of kernel.</p>
+       </td>
+                   </tr>
+                 </table>
+                 <table id="note2" class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE  2:</td>
+                     <td valign="top" class="Note">
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83a">These results are based on a study carried out on three different types of kernel.</p>
+       </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -143,17 +166,23 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="" class="Note">
-                   <p class="Note"><span class="note_label">NOTE</span>&#160; These results are based on a study carried out on three different types of kernel.</p>
-                   <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83a">These results are based on a study carried out on three different types of kernel.</p>
-                 </div>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <table class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE:</td>
+                     <td valign="top" class="Note">
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83f">These results are based on a study carried out on three different types of kernel.</p>
+         <p class="Note" id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83a">These results are based on a study carried out on three different types of kernel.</p>
+       </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -176,7 +205,7 @@ INPUT
     </foreword></preface>
     </gb-standard>
     INPUT
-           <html xmlns:epub="http://www.idpf.org/2007/ops">
+       <html xmlns:epub="http://www.idpf.org/2007/ops">
          <head>
            <title>test</title>
            <body lang="EN-US" link="blue" vlink="#954F72">
@@ -187,23 +216,28 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="" class="Note"><p class="Note"><span class="note_label">NOTE</span>&#160; </p>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <table class="Note">
+                   <tr>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">NOTE:</td>
+                     <td valign="top" class="Note">
            <dl><dt><p class="Note">A</p></dt><dd><p class="Note">B</p></dd></dl>
            <ul>
            <li>C</li></ul>
-       </div>
+       </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
        </html>
-
     OUTPUT
   end
 
@@ -228,10 +262,10 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
                  <table id="samplecode" class="example">
                    <tr>
-                     <td width="110pt" valign="top" class="example_label" style="width:82.8pt;padding:.75pt .75pt .75pt .75pt">EXAMPLE</td>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">EXAMPLE:</td>
                      <td valign="top">
          <p>Hello</p>
        </td>
@@ -242,7 +276,8 @@ INPUT
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -275,10 +310,10 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
                  <table id="samplecode" class="example">
                    <tr>
-                     <td width="110pt" valign="top" class="example_label" style="width:82.8pt;padding:.75pt .75pt .75pt .75pt">EXAMPLE  1</td>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">EXAMPLE  1:</td>
                      <td valign="top">
          <p>Hello</p>
        </td>
@@ -286,7 +321,7 @@ INPUT
                  </table>
                  <table id="samplecode2&gt;   " class="example">
                    <tr>
-                     <td width="110pt" valign="top" class="example_label" style="width:82.8pt;padding:.75pt .75pt .75pt .75pt">EXAMPLE  2</td>
+                     <td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">EXAMPLE  2:</td>
                      <td valign="top"/>
                    </tr>
                  </table>
@@ -296,7 +331,8 @@ INPUT
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -332,23 +368,27 @@ INPUT
              <div class="WordSection2">
                <br/>
                <div>
-                 <h1 class="ForewordTitle">Foreword</h1>
-                 <div id="_be9158af-7e93-4ee2-90c5-26d31c181934" class="formula"><span class="stem">(#(r = 1 %)#)</span>&#160; (1)</div>
+                 <h1 class="ForewordTitle">Foreword&#160;</h1>
+                 <div id="_be9158af-7e93-4ee2-90c5-26d31c181934" class="formula">&#160; <span class="stem">(#(r = 1 %)#)</span>&#160; (1)</div>
                  <p>where</p>
-                 <dl>
-                   <dt>
+                 <table class="dl">
+                   <tr>
+                     <td valign="top" align="left">
            <span class="stem">(#(r)#)</span>
-         </dt>
-                   <dd>
+         </td>
+                     <td valign="top">&#8212;</td>
+                     <td valign="top">
            <p id="_1b99995d-ff03-40f5-8f2e-ab9665a69b77">is the repeatability limit.</p>
-         </dd>
-                 </dl>
+         </td>
+                   </tr>
+                 </table>
                </div>
                <p>&#160;</p>
              </div>
              <br/>
              <div class="WordSection3">
-               <p class="zzSTDTitle1"/>
+               <p class="zzSTDTitle1">XXXX</p>
+               <hr width="25%"/>
              </div>
            </body>
          </head>
@@ -357,7 +397,7 @@ INPUT
   end
 
     it "processes term notes" do
-    expect(IsoDoc::Convert.new({}).convert_file(<<~"INPUT", "test", true)).to be_equivalent_to <<~"OUTPUT"
+    expect(Asciidoctor::Gb::GbConvert.new({}).convert_file(<<~"INPUT", "test", true)).to be_equivalent_to <<~"OUTPUT"
         <gb-standard xmlns="http://riboseinc.com/gbstandard">
     <sections>
     <terms id="_terms_and_definitions" obligation="normative"><title>Terms and Definitions</title>
@@ -376,6 +416,37 @@ INPUT
 </sections>
 </gb-standard>
     INPUT
+           <html xmlns:epub="http://www.idpf.org/2007/ops">
+         <head>
+           <title>test</title>
+           <body lang="EN-US" link="blue" vlink="#954F72">
+             <div class="WordSection1">
+               <p>&#160;</p>
+             </div>
+             <br/>
+             <div class="WordSection2">
+               <p>&#160;</p>
+             </div>
+             <br/>
+             <div class="WordSection3">
+               <p class="zzSTDTitle1">XXXX</p>
+               <div id="_terms_and_definitions"><h1>1.&#12288;Terms and Definitions</h1><p>For the purposes of this document,
+           the following terms and definitions apply.</p>
+       <p class="TermNum" id="paddy">1.1</p><p class="Terms" style="text-align:left;">paddy</p><p class="AltTerms" style="text-align:left;">paddy rice</p>
+       <p id="_eb29b35e-123e-4d1c-b50b-2714d41e747f">rice retaining its husk after threshing</p>
+       <table id="_671a1994-4783-40d0-bc81-987d06ffb74e" class="Note"><tr><td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">Note 1 to entry:</td><td valign="top" class="Note">
+         <p class="Note" id="_19830f33-e46c-42cc-94ca-a5ef101132d5">The starch of waxy rice consists almost entirely of amylopectin. The kernels have a tendency to stick together after cooking.</p>
+       </td></tr></table>
+       <table id="_671a1994-4783-40d0-bc81-987d06ffb74f" class="Note"><tr><td valign="top" class="example_label" style="padding:2pt 2pt 2pt 2pt">Note 2 to entry:</td><td valign="top" class="Note">
+       <ul><li>A</li></ul>
+         <p class="Note" id="_19830f33-e46c-42cc-94ca-a5ef101132d5">The starch of waxy rice consists almost entirely of amylopectin. The kernels have a tendency to stick together after cooking.</p>
+       </td></tr></table>
+       </div>
+               <hr width="25%"/>
+             </div>
+           </body>
+         </head>
+       </html>
 OUTPUT
     end
 end
