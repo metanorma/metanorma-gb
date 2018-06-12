cd365c0b8120a705977924ff29a7a80bc96a9f1d
Nick Nicholas <opoudjis@optusnet.com.au>
2018-06-13 09:55:55 +1000

    efface WordConvertModule

--- lib/isodoc/gb/gbwordconvert.rb
+++ lib/isodoc/gb/gbwordconvert.rb
@@ -11,7 +11,6 @@ module IsoDoc
     # schema encapsulation of the document for validation
 
     class WordConvert < IsoDoc::WordConvert
-      #include IsoDoc::WordConvertModule
       def default_fonts(options)
         script = options[:script] || "Hans"
         b = options[:bodyfont] ||
