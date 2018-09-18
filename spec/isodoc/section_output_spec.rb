24ef4766351c278496a6d6ee212b62626e52c1dd
Nick Nicholas <opoudjis@optusnet.com.au>
2018-09-18 22:06:03 +1000

    option to suppress heading numbers

--- spec/isodoc/section_output_spec.rb
+++ spec/isodoc/section_output_spec.rb
@@ -58,6 +58,40 @@ RSpec.describe IsoDoc::Gb::HtmlConvert do
     OUTPUT
   end
 
+        it "processes clause names, suppressing heading numbers" do
+          expect(IsoDoc::Gb::HtmlConvert.new({suppressheadingnumbers: true}).convert("test", <<~"INPUT", true).gsub(/^.*<body/m, "<body").gsub(%r{</body>.*}m, "</body>")).to be_equivalent_to <<~"OUTPUT"
+        <gb-standard xmlns="http://riboseinc.com/gbstandard">
+        <bibdata>
+        <language>en</language>
+        <script>Latn</script>
+        </bibdata>
+               <sections>
+               <clause id="M" inline-header="false" obligation="normative"><title>Clause 4</title><clause id="N" inline-header="false" obligation="normative">
+         <title>Introduction</title>
+       </clause>
+       <clause id="O" inline-header="true" obligation="normative">
+         <title>Clause 4.2</title>
+       </clause></clause>
+        </sections>
+    </gb-standard>
+    INPUT
+    #{HTML_HDR}
+               <p class="zzSTDTitle1">XXXX</p>
+                     <div id="M">
+        <h1>Clause 4</h1>
+        <div id="N">
+   <h2>Introduction</h2>
+ </div>
+        <div id="O">
+   <span class="zzMoveToFollowing"><b>Clause 4.2 </b></span>
+ </div>
+      </div>
+               <hr width="25%"/>
+             </div>
+           </body>
+    OUTPUT
+  end
+
         it "processes annex names" do
           expect(IsoDoc::Gb::HtmlConvert.new({}).convert("test", <<~"INPUT", true).gsub(/^.*<body/m, "<body").gsub(%r{</body>.*}m, "</body>")).to be_equivalent_to <<~"OUTPUT"
         <gb-standard xmlns="http://riboseinc.com/gbstandard">
