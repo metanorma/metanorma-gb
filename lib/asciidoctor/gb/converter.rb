d07cbf293f2817a0fe0051a9cea78c814098a75e
Nick Nicholas <opoudjis@optusnet.com.au>
2019-03-21 23:34:34 +1100

    conditional validation; https://github.com/metanorma/metanorma-standoc/issues/44

--- lib/asciidoctor/gb/converter.rb
+++ lib/asciidoctor/gb/converter.rb
@@ -28,7 +28,7 @@ module Asciidoctor
         result << "</gb-standard>"
         result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
-        validate(ret1)
+        validate(ret1) unless @novalid
         ret1.root.add_namespace(nil, GB_NAMESPACE)
         ret1
       end
