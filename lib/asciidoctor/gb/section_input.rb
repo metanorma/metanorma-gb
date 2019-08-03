a1ef21f081c4e758dfbc00165f260d5b863db827
Nick Nicholas <opoudjis@gmail.com>
2019-08-03 23:54:12 +1000

    clauses can have language and script; https://github.com/metanorma/metanorma-standoc/issues/28

--- lib/asciidoctor/gb/section_input.rb
+++ lib/asciidoctor/gb/section_input.rb
@@ -23,7 +23,7 @@ module Asciidoctor
       end
 
       def section(node)
-        a = { id: Asciidoctor::Standoc::Utils::anchor_or_uuid(node) }
+        a = section_attributes(node)
         noko do |xml|
           case sectiontype(node)
           when "引言", "introduction" then introduction_parse(a, xml, node)
