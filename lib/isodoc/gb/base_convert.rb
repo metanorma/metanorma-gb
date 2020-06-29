2ab28e27cdf9654e5cbcbaa3ca303f4a96be4a25
Nick Nicholas <opoudjis@gmail.com>
2020-06-29 23:23:53 +1000

    i18n refactor: https://github.com/metanorma/metanorma-standoc/issues/285

--- lib/isodoc/gb/base_convert.rb
+++ lib/isodoc/gb/base_convert.rb
@@ -27,20 +27,6 @@ module IsoDoc
         @cleanup.example_cleanup(docxml)
       end
 
-      def i18n_init(lang, script)
-        super
-        y = if lang == "en"
-              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
-            elsif lang == "zh" && script == "Hans"
-              YAML.load_file(File.join(File.dirname(__FILE__),
-                                       "i18n-zh-Hans.yaml"))
-            else
-              YAML.load_file(File.join(File.dirname(__FILE__),
-                                       "i18n-zh-Hans.yaml"))
-            end
-        @labels = @labels.merge(y)
-      end
-
       def omit_docid_prefix(prefix)
         super || prefix == "Chinese Standard"
       end
--- lib/isodoc/gb/init.rb
+++ lib/isodoc/gb/init.rb
@@ -18,6 +18,20 @@ module IsoDoc
       def xref_init(lang, script, klass, labels, options)
         @xrefs = Xref.new(lang, script, klass, labels, options)
       end
+
+      def i18n_init(lang, script)
+        super
+        y = if lang == "en"
+              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
+            elsif lang == "zh" && script == "Hans"
+              YAML.load_file(File.join(File.dirname(__FILE__),
+                                       "i18n-zh-Hans.yaml"))
+            else
+              YAML.load_file(File.join(File.dirname(__FILE__),
+                                       "i18n-zh-Hans.yaml"))
+            end
+        @labels = @labels.merge(y)
+      end
     end
   end
 end
